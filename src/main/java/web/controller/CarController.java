04767b2e5ec7a2ea27df0eefd9d418df9845678e
Kadavercian01 <indigo.gamer.life@gmail.com>
2023-07-31 20:01:04 +0300

    fix

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,11 @@ import web.service.CarServiceImpl;
 @Controller
 public class CarController {
 
-    private CarService carService = new CarServiceImpl();
+    private final CarService carService;
+    @Autowired
+    public CarController(CarServiceImpl carServiceImp) {
+        this.carService = carServiceImp;
+    }
 
     @GetMapping("/car")
     public String getCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,10 +1,11 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.model.Car;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Service
 public class CarServiceImpl implements CarService {
     private List<Car> cars; {
         cars = new ArrayList<>();
